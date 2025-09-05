17a657b32c2dbb92db383ab2ceebcab79f6c3465
Henrique Pagliarini <henrique.dev.p@outlook.com>
2025-09-05 02:14:17 -0300

    refactor: Remoção de arquivo antigo e enum de teste no QueueNames

--- QueueNames.py
+++ QueueNames.py
@@ -6,8 +6,6 @@ class QueueNames(Enum):
     BID_DONE = "lance_realizado"
     BID_VALIDATED = "lance_validado"
     AUCTION_WINNER = "leilao_vencedor"
-    AUCTION_1 = "leilao_1"
-    AUCTION_2 = "leilao_2"
 
     def __str__(self):
         return self.value
