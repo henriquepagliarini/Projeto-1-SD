0b00fc6e7f53e5e6de413c7fb107ba2064a46dd7
Henrique Pagliarini <henrique.dev.p@outlook.com>
2025-09-05 02:10:11 -0300

    feat: Pequena mudança na lógica do rabbitmq

--- RabbitMQConnection.py
+++ RabbitMQConnection.py
@@ -3,8 +3,9 @@ import pika
 
 class RabbitMQConnection:
     def __init__(self):
-        self.connection: None
-        self.channel: None
+        self.connection = None
+        self.channel = None
+        self.exchange = None
         self.isOnline = False
 
     def connect(self):
@@ -24,12 +25,17 @@ class RabbitMQConnection:
             print("Conexão fechada.")
     
     def setupExchange(self, exchange: str):
+        self.exchange = exchange
         self.channel.exchange_declare(
             exchange=exchange, 
             exchange_type='direct', 
             durable=True
         )
         print(f"Exchange '{exchange}' criada.")
+
+    def setupRabbitQueues(self, exchange: str, queue_and_routingkey_string: str):
+        self.setupQueue(exchange, queue_and_routingkey_string, queue_and_routingkey_string)
+        print(f"Fila '{queue_and_routingkey_string}' iniciada.")
 
     def setupQueue(self, exchange: str, queue: str, routing_key: str):
         self.channel.queue_declare(
@@ -42,4 +48,3 @@ class RabbitMQConnection:
             queue=queue,
             routing_key=routing_key
         )
-        print(f"Fila '{queue}' iniciada.")
